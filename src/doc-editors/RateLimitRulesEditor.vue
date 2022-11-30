75d70839e768b91b2016cffe5dca0857d623b709
AylonS <aylons@reblaze.com>
2022-11-30 11:17:30 +0200

    kebab case
    Signed-off-by: AylonS <aylons@reblaze.com>

--- src/doc-editors/RateLimitRulesEditor.vue
+++ src/doc-editors/RateLimitRulesEditor.vue
@@ -249,7 +249,6 @@
       <security-policies-connections
           selectedDocType="ratelimits"
           :selectedDocId="localDoc.id"
-          @havePoliciesConnections="emitConnections"
           @go-to-route="emitGoToRoute"
           @have-policies-connections="emitConnections"
           :selectedBranch="selectedBranch">
