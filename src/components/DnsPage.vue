fcb7f2c83c52123b8261cf5f0989125800ccec06
AylonS <aylons@reblaze.com>
2022-12-04 09:24:32 +0200

    improving dns table
    Signed-off-by: AylonS <aylons@reblaze.com>

--- src/components/DnsPage.vue
+++ src/components/DnsPage.vue
@@ -24,7 +24,7 @@
 import {defineComponent} from 'vue'
 import RbzTable from '@/components/RbzTable.vue'
 import {ColumnOptions, DnsRecord} from '@/types'
-// import RequestsUtils from '@/assets/RequestsUtils'
+import RequestsUtils from '@/assets/RequestsUtils'
 
 
 export default defineComponent({
@@ -85,26 +85,10 @@ export default defineComponent({
 
     async loadDNS() {
       this.setLoadingDocStatus(true)
-      // const url = 'tools/dns-information/'
-      // const methodName = 'GET'
-      // const response = await RequestsUtils.sendReblazeRequest({methodName, url})
-      // this.dnsRecords = response?.data?.dns_records || []
-
-      this.dnsRecords = [
-        {
-          name: 'rbzdevay001olbs.dev.rbzdns.com',
-          resource_records: ['asdf23421342sadf', 'asdfg dsdfsadf', 'asdf6453645sadf',
-            'asd34564356435fsadf', 'asdfsadf'],
-          ttl: 99999,
-          type: 'NS',
-        },
-        {
-          name: 'rbzdevay001olbs.dev.rbzdns.com',
-          resource_records: ['asdf234213df6453645s.dfgdfgdfgsdgsdd.sddfsadf'],
-          ttl: 99999,
-          type: 'SOA',
-        },
-      ]
+      const url = 'tools/dns-information/'
+      const methodName = 'GET'
+      const response = await RequestsUtils.sendReblazeRequest({methodName, url})
+      this.dnsRecords = response?.data?.dns_records || []
       this.setLoadingDocStatus(false)
     },
   },
